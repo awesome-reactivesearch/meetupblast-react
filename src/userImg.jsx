28e620875b6fd80d28080c3b1280c4ebd3f952ce
Deepak Grover <deepakgrover333@gmail.com>
2017-09-20 02:14:56 +0530

    Fix memory leakage issue

--- src/userImg.jsx
+++ src/userImg.jsx
@@ -1,26 +1,16 @@
 var React = require('react');
 
 var UserImg = React.createClass({
-    componentDidMount: function() {
-        var self = this;
-        this.img = new Image();
-        var defaultSrc = 'http://www.avidog.com/wp-content/uploads/2015/01/BellaHead082712_11-50x65.jpg';
-        this.img.onerror = function() {
-            if (self.isMounted()) {
-                self.setState({
-                    src: defaultSrc
-                });
-            }
-        };
-        this.img.src = this.state.src;
-    },
     getInitialState: function() {
         return {
             src: this.props.src
         };
     },
     render: function() {
-        return <img src={this.state.src} />;
+        return <img 
+            onError={() => this.setState({src: 'http://www.avidog.com/wp-content/uploads/2015/01/BellaHead082712_11-50x65.jpg'})}
+            src={this.state.src}
+        />
     }
 });
 module.exports = UserImg;
